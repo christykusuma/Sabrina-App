213e2020d6c41138009561a1576ab081038f1020
Christy Kusuma <christykusuma@Christys-MacBook-Pro.local>
2019-06-04 16:53:43 +0700

    created friendlists using album json data from demo (will be replaced later)

--- index.ios.js
+++ index.ios.js
@@ -4,7 +4,7 @@ import Header from './src/components/Header';
 import FriendList from './src/components/FriendList';
 
 const App = () => (
-    <View>
+    <View style={{ flex: 1 }}>
         <Header headerText={'Sabrina'} />
         < FriendList />
     </View>
